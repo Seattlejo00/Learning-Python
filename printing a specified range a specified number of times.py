e3fd97446ab11e0adc94f336009d48cdff388890
James O'Connor <101301931+Seattlejo00@users.noreply.github.com>
2022-03-14 22:45:14 -0400

    Add files via upload

---
+++ printing a specified range a specified number of times.py
@@ -0,0 +1,16 @@
+
+
+#Getting user inputs for range floor and ceiling
+minima = int(input("Enter range minima: "))
+premaxima = int(input("Enter range maxima: "))
+#The for loop will exclude the maxima so we add one
+maxima = premaxima + 1
+
+#Getting the desired number of prints from the user
+input = int(input("Enter number of prints: "))
+i = 1
+while i <= input:
+    i = i + 1
+    for index in range(minima, maxima):
+        print(index)
+print("Done!")
